ae704675d0ee9b81b860a02ab312aaf1e6faa391
brendolalves <brendol.alves@gmail.com>
2026-06-10 01:19:55 -0300

    download do folheto

--- app.py
+++ app.py
@@ -13,3 +13,20 @@ EMAIL_PASS = os.getenv('EMAIL_PASS')
 
 hoje = datetime.now()
 data = hoje.strftime("%d/%m/%Y")
+
+class Folheto:
+    def __init__(self):
+        self.savegnago_url = "https://imagens.savegnagoonline.com.br/jornal-semanal-app/Rio_Claro.pdf"
+
+    def baixar_folheto(self, nome_arquivo="ofertas.pdf"):
+        try:
+            res = requests.get(self.savegnago_url, headers={'User-Agent': 'Mozilla/5.0'})
+            if res.status_code == 200:
+                with open(nome_arquivo, "wb") as f:
+                    f.write(res.content)
+                return True
+            return False
+        except Exception as e:
+            print(f"Erro ao baixar o PDF: {e}")
+            return False
+        
